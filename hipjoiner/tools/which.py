ef4faf5d635a32ab9a88f224c4833a11382b137b
John Pirie <john@thepiries.net>
2019-10-20 08:40:26 -0400

    Fix which; no dupe separators

--- hipjoiner/tools/which.py
+++ hipjoiner/tools/which.py
@@ -10,12 +10,13 @@ def which(arg=None):
         arg = sys.argv[1]
     result = []
     dirs = os.environ['PATH'].split(os.pathsep)
-    for dir in dirs:
-        if not os.path.isdir(dir):
+    for d in dirs:
+        d = d.rstrip(os.path.sep)
+        if not os.path.isdir(d):
             continue
-        files = os.listdir(dir)
+        files = os.listdir(d)
         for fname in files:
-            fpath = '%s\%s' % (dir, fname)
+            fpath = '%s\%s' % (d, fname)
             if not os.path.isfile(fpath):
                 continue
             if arg == fname.split('.')[0]:
@@ -25,7 +26,10 @@ def which(arg=None):
 
 
 if __name__ == '__main__':
+    which('ppath')
+    """
     if len(sys.argv) < 2:
         print('Usage: which <term>')
         sys.exit(0)
     which(sys.argv[1])
+    """
